6933edb8de1dc89ece401489bbf917418aba712c
Piotr Kuglin <kuglinpiotr@gmail.com>
2019-03-13 00:24:58 +0100

    initial commit

---
+++ path/path_base.cpp
@@ -0,0 +1,16 @@
+#include "path_base.h"
+
+
+
+path_base::path_base()
+{
+}
+
+path_base::path_base(int sizeX, int sizeY, int** map) :sizeX(sizeX), sizeY(sizeY), map(map)
+{
+}
+
+
+path_base::~path_base()
+{
+}
