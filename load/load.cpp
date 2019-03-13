764d5338d774d066cb3f3005e5e1e4a182eb72fe
lukaszkolarz <lukaszkolarz@gmail.com>
2019-03-13 13:01:22 +0100

    create new class

---
+++ load/load.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Łukasz Kolarz on 2019-03-13.
+//
+
+#include "load.h"
---
+++ load/load.h
@@ -0,0 +1,14 @@
+//
+// Created by Łukasz Kolarz on 2019-03-13.
+//
+
+#ifndef ROBO_LOAD_H
+#define ROBO_LOAD_H
+
+
+class load {
+
+};
+
+
+#endif //ROBO_LOAD_H
