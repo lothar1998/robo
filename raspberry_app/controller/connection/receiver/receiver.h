e5c327faf5b82c5442396d3c3816bdacc2f63560
Piotr Kuglin <kuglinpiotr@gmail.com>
2019-04-24 17:49:50 +0200

    several improvements

--- raspberry_app/controller/connection/receiver/receiver.h
+++ raspberry_app/controller/connection/receiver/receiver.h
@@ -12,13 +12,46 @@
 #include <cerrno>
 #include <cstring>
 #include <iostream>
+#include <exception>
 
 using namespace std;
+
+
+struct socketException: public exception{
+    const char * what() const noexcept override {
+        return "socket creation exception";
+    }
+};
+
+struct bindException: public exception{
+    const char * what() const noexcept override {
+        return "bind socket exception";
+    }
+};
+
+struct listenException: public exception{
+    const char * what() const noexcept override {
+        return "listen on socket exception";
+    }
+};
+
+struct acceptException: public exception{
+    const char * what() const noexcept override {
+        return "accept client exception";
+    }
+};
+
+struct shutdownException: public exception{
+    const char * what() const noexcept override {
+        return "shutdown socket exception";
+    }
+};
+
 
 template <typename T, class B>
 class receiver {
 public:
-    receiver(B *, void (B::*)(T),T,int,string = "127.0.0.1",int = AF_INET,int = SOCK_STREAM,int = 0);
+    receiver(B *, void (B::*)(T),T,size_t ,int,string = "127.0.0.1",int = AF_INET,int = SOCK_STREAM,int = 0);
     ~receiver();
     void operator()();
 
@@ -36,14 +69,17 @@ private:
     struct sockaddr_in clientAddress;
     size_t clientAddressSize;
 
-    T buffer;//TODO maybe array or something
+    T * buffer;
+    size_t bufferSize;
 };
 
 template <typename T,class B>
-receiver<T,B>::receiver(B * obj, void (B::*fun)(T), T stopCondition, int port, string ip_addr, int domain, int type, int protocol):fun_ptr(fun),stopCondition(stopCondition),obj(obj){
+receiver<T,B>::receiver(B * obj, void (B::*fun)(T), T stopCondition, size_t bufferSize, int port, string ip_addr, int domain, int type, int protocol):fun_ptr(fun),stopCondition(stopCondition),obj(obj),bufferSize(bufferSize){
+
+    buffer = new T[bufferSize];
 
     if((socketHandle=socket(domain,type,protocol))<0)
-        cout<<strerror(errno);
+        throw socketException();
 
     address.sin_family = domain;
     inet_pton(domain,ip_addr.c_str(),&address.sin_addr);
@@ -51,38 +87,40 @@ receiver<T,B>::receiver(B * obj, void (B::*fun)(T), T stopCondition, int port, s
 
 
     if((bind(socketHandle,(struct sockaddr *)&address,sizeof(address)))<0)
-        cout<<strerror(errno);
+        throw bindException();
 }
 
 template <typename T, class B>
 receiver<T,B>::~receiver() {
 
     if(shutdown(clientSocketHandle,SHUT_RDWR)<0)
-        cout<<strerror(errno);
+        throw shutdownException();
 
 
     if(shutdown(socketHandle,SHUT_RDWR)<0)
-        cout<<strerror(errno);
+        throw shutdownException();
 }
 
 template <typename T, class B>
 void receiver<T,B>::operator()() {
 
     if (listen(socketHandle, 1) < 0)
-        cout << strerror(errno);
-
-
-    if ((clientSocketHandle = accept(socketHandle, (struct sockaddr *) &clientAddress, (socklen_t *) &clientAddressSize)) < 0)
-        cout << strerror(errno);
-
+        throw listenException();
 
     do {
 
-        if (recv(clientSocketHandle, &buffer, sizeof(T), MSG_WAITALL) > 0)
-            (obj->*fun_ptr)(buffer);
+        if ((clientSocketHandle = accept(socketHandle, (struct sockaddr *) &clientAddress, (socklen_t * ) & clientAddressSize)) < 0)
+            throw acceptException();
 
-    } while (buffer != stopCondition);
+        do {
 
+            if (recv(clientSocketHandle, &buffer, sizeof(T)*bufferSize, MSG_WAITALL) > 0)
+                (obj->*fun_ptr)(buffer);
+            else
+                break;
+
+        } while (buffer != stopCondition);
+    }while (buffer != stopCondition);
 }
 
 
